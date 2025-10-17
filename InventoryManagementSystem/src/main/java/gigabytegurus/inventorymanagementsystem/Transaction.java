d566662fbff899b6fd719c70386792a65475a8a3
BMartin00 <martinbanyanszki00@gmail.com>
2025-10-17 10:46:54 +0100

    Added a transaction class.

---
+++ InventoryManagementSystem/src/main/java/gigabytegurus/inventorymanagementsystem/Transaction.java
@@ -0,0 +1,22 @@
+package gigabytegurus.inventorymanagementsystem;
+
+import java.util.Date;
+
+public class Transaction
+{
+	private int transactionId;
+	private Item item;
+	private int quantity;
+	private String type;
+	private Date date;
+	
+	public void processSale()
+	{
+		
+	}
+	
+	public void processReturn()
+	{
+		
+	}
+}
